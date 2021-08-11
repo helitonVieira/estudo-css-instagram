65cefc33c061e0e645efadf6df68330f8fbfa234
heliton Vieira <helitondba@gmail.com>
2021-08-11 16:22:10 -0300

    retirando o js

--- js/main.js
+++
@@ -1,5 +0,0 @@
-alert("Meu primeiro js chamada via novo arquivo");
-var nome = 'Heliton Silva Vieira';
-var idade = 38;
-var idade2 = 22;
-alert('Meu nome é: '+ nome + ' e tenho'+ idade +  ' anos de idade' + 'soma: '+ idade + idade2);
